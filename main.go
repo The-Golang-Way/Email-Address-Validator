d101bdbdebe0b0b46018fa65846caacaf52de6e9
Yehdar <p.radhey06@gmail.com>
2023-06-19 14:04:27 -0400

    added function that searches the DNS MX records

--- main.go
+++ main.go
@@ -3,10 +3,21 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"net"
 	"os"
 )
 
 func main(){
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("domain\n")
+	fmt.Println("hasMX\n")
+
+	for scanner.Scan(){
+		checkDomain(scanner.Text())
+	}
+}
+
+func checkDomain(domain string){
+	var hasMX bool
+
+	mxRecords, err := net.LookupMX(domain)
 }
