8507207cd3121f7ec1efa873a73d111bb2eeb672
Yehdar <p.radhey06@gmail.com>
2023-06-21 11:07:11 -0400

    checked DMARC records for extra validation

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main(){
 }
 
 func checkDomain(domain string){
-	var hasMX, hasSPF bool
+	var hasMX, hasSPF, hasDMARC bool
 
 	// checking MX records
 	mxRecords, err := net.LookupMX(domain)
@@ -27,7 +27,7 @@ func checkDomain(domain string){
 		os.Exit(1)
 	}
 	if len(mxRecords) > 0 {
-		hasMx = true
+		hasMX = true
 	}
 
 	txtRecords, err := net.LookupTXT(domain)
@@ -42,4 +42,19 @@ func checkDomain(domain string){
 			break
 		}
 	}
+
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(3)
+	}
+
+	for _, record := range dmarcRecords{
+		if strings.HasPrefix(record, "v=DMARC1"){
+			hasDMARC = true
+			break
+		}
+	}
+
+	fmt.Printf("%v, %v, %v, %v", domain, hasMX, hasSPF, hasDMARC)
 }
